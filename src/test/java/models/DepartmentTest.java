5ef28d2502cf5cedfd170c9b6ef4baafc9259f79
Barsil Nzola <barsiljohn@gmail.com>
2020-07-20 09:03:51 +0300

    generated test methods for the getters and setters of the department class and ran them to fail

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,7 +1,51 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DepartmentTest {
 
+    @Test
+    public void getNameReturnsCorrectName() {
+        Department testDepartment = setupDepartment();
+        assertEquals("Human Resource", testDepartment.getName());
+    }
+
+    @Test
+    public void setNameSetsCorrectName() {
+        Department testDepartment = setupDepartment();
+        testDepartment.setName("Finance");
+        assertNotEquals("Human Resource", testDepartment.getName());
+    }
+
+    @Test
+    public void getDescriptionReturnsCorrectDescription() {
+        Department testDepartment = setupDepartment();
+        assertEquals("employee management", testDepartment.getDescription);
+    }
+
+    @Test
+    public void setDescriptionSetsCorrectDescription() {
+        Department testDepartment = setupDepartment();
+        testDepartment.setDescription("financial management");
+        assertNotEquals("employee management", testDepartment.getDescription);
+    }
+
+    @Test
+    public void getNoOfEmployeesgetsCorrectNumber() {
+        Department testDepartment = setupDepartment();
+        assertEquals(7, testDepartment.getNoOfEmployees);
+    }
+
+    @Test
+    public void setNoOfEmployeesSetsCorrectNumber() {
+        Department testDepartment = setupDepartment();
+        testDepartment.setNoOfEmployees(8);
+        assertNotEquals(7,testDepartment.getNoEmployees);
+    }
+
+    public Department setupDepartment () {
+        return new Department("Human Resource", "employee management", 7);
+    }
 }
