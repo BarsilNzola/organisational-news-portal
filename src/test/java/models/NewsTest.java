e90cd2d35ce3eff9bcb0e39f8f7a1d294b5b555f
Barsil Nzola <barsiljohn@gmail.com>
2020-07-20 21:47:31 +0300

    generated test methods for the getters and setters for the news class

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,7 +1,38 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class NewsTest {
 
+    @Test
+    public void getPostByReturnsCorrectPost() {
+        News testNews = setUpNews();
+        assertEquals("Salary Decreament", testNews.getPost());
+    }
+
+    @Test
+    public void setPostSetsCorrectPost() {
+        News testNews = setUpNews();
+        testNews.setPost("client added features");
+        assertNotEquals("Salary Decreament", testNews.getPost());
+    }
+
+    @Test
+    public void getPostedByReturnsCorrectPostedBy() {
+        News testNews = setUpNews();
+        assertEquals("Jackqueen", testNews.getPostedBy());
+    }
+
+    @Test
+    public void setPostedBySetsCorrectPostedBy() {
+        News testNews = setUpNews();
+        testNews.setPostedBy("Barsil");
+        assertNotEquals("Jackqueen", testNews.setPostedBy());
+    }
+
+    public News setUpNews() {
+        return new News("Salary Decreament", "Jackqueen");
+    }
 }
